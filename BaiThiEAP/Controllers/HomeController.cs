5780e67e00cb5376460a41816dfa761e02421c2d
nam159198 <namkun159198@gmail.com>
2020-05-07 19:44:00 +0700

    WebMVC

---
+++ BaiThiEAP/Controllers/HomeController.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using BaiThiEAP.Models;
+
+namespace BaiThiEAP.Controllers
+{
+    public class HomeController : Controller
+    {
+        public static Account acResult = new Account();
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult About()
+        {
+            return View(acResult);
+        }
+        public ActionResult Tranfers()
+        {
+            
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Tranfers(Account ac)
+        {
+            var account = new ServiceReference1.Service1Client();
+            var result = account.GetAccount(ac.Code, ac.Password);
+            if (result != null)
+            {
+                acResult.Username = result.Username;
+                acResult.Blance = result.Blance;
+                return RedirectToAction("About",acResult);
+            }
+            else
+            {
+                ViewBag.error = "Account not Exist";
+                return View();
+            }
+            
+        }
+
+       
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ BaiThiEAP/Models/Account.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace BaiThiEAP.Models
+{
+    public class Account
+    {
+        [Required]
+        public int Code { get; set; }
+        [Required]
+        [DataType(DataType.Password)]
+        public String Password { get; set; }
+        public String Username { get; set; }
+        public decimal Blance { get; set; }
+    }
+}
